54d349c1c623178b2b510185a0aa0f916ca0d37c
Manish Kumar <manish.login01@gmail.com>
2026-07-15 20:56:26 +0530

    Refactor App component and remove unused CSS styles

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+const rootElement = document.getElementById("root");
+if (!rootElement) throw new Error("Root element not found");
+
+createRoot(rootElement).render(
     <App />
-  </StrictMode>,
-)
+);
